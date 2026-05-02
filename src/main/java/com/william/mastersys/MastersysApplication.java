76cca5b56bafda6069a6a42632566e7df0f122a2
wkoju84 <wkoju84@gmail.com>
2026-05-02 14:45:01 -0300

    commit inicial

---
+++ src/main/java/com/william/mastersys/MastersysApplication.java
@@ -0,0 +1,13 @@
+package com.william.mastersys;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MastersysApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MastersysApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/william/mastersys/MastersysApplicationTests.java
@@ -0,0 +1,13 @@
+package com.william.mastersys;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MastersysApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
