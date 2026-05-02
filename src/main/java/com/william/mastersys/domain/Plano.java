92fa26f121803182f769840e21eda4b6afd9f833
wkoju84 <wkoju84@gmail.com>
2026-05-02 17:37:25 -0300

    Adicionando classe Plano

---
+++ src/main/java/com/william/mastersys/domain/Plano.java
@@ -0,0 +1,54 @@
+package com.william.mastersys.domain;
+
+import jakarta.persistence.*;
+
+import java.math.BigDecimal;
+
+@Entity
+@Table(name = "planos")
+public class Plano {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nome;
+
+    @Column(name = "valor_mensal")
+    private BigDecimal valorMensal;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "modalidade_id")
+    private Modalidade modalidade;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public BigDecimal getValorMensal() {
+        return valorMensal;
+    }
+
+    public void setValorMensal(BigDecimal valorMensal) {
+        this.valorMensal = valorMensal;
+    }
+
+    public Modalidade getModalidade() {
+        return modalidade;
+    }
+
+    public void setModalidade(Modalidade modalidade) {
+        this.modalidade = modalidade;
+    }
+}
